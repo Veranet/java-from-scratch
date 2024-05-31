7dcd1471db997a1ba2591540e11cdfa81b7dabb2
Veranet <halatsiankova@gmail.com>
2024-05-31 11:45:58 +0200

    protection against empty parameters

--- hello-world-app/src/main/java/halatsiankova/javafromscratch/lesson1/HelloWorld.java
+++ hello-world-app/src/main/java/halatsiankova/javafromscratch/lesson1/HelloWorld.java
@@ -7,12 +7,17 @@ public class HelloWorld {
      * @param args - username
      */
     public static void main(String[] args) {
-        var name = args[0];
-        var welcomePhrase = new HelloWorld().sayHello(name);
+        var helloWorld = new HelloWorld();
+        var name = helloWorld.isParamPresent(args) ? args[0] : "World";
+        var welcomePhrase = helloWorld.sayHello(name);
         System.out.println(welcomePhrase);
     }
 
     private String sayHello(String name) {
         return String.format("Hello %s!", name);
+    }
+
+    private boolean isParamPresent(String[] args) {
+        return args != null && args.length != 0;
     }
 }
