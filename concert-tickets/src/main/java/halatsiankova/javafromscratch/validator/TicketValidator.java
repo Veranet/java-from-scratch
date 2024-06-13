c8222e31b0718cdc8a63d1fb9a651f95d159991b
Veranet <halatsiankova@gmail.com>
2024-06-13 22:53:11 +0200

    lesson-4 fix validation logs

--- concert-tickets/src/main/java/halatsiankova/javafromscratch/validator/TicketValidator.java
+++ concert-tickets/src/main/java/halatsiankova/javafromscratch/validator/TicketValidator.java
@@ -7,42 +7,49 @@ public class TicketValidator {
     public void validateTicketId(String ticketId) {
         var ticketIdLength = ticketId.length();
         if (ticketIdLength > 4) {
-            throw new IllegalArgumentException("ticketId must contains max 4 digits and/or chars.");
+            var errorLog = String.format("ticketId=%s must contains max 4 digits and/or chars.", ticketId);
+            throw new IllegalArgumentException(errorLog);
         }
         String pattern = "[a-zA-Z0-9]{%d}".formatted(ticketIdLength);
         Pattern compiledPattern = Pattern.compile(pattern);
         if (!compiledPattern.matcher(ticketId).find()) {
-            throw new IllegalArgumentException("ticketId must consist of numbers and/or symbols only.");
+            var errorLog = String.format("ticketId=%s must consist of numbers and/or symbols only.", ticketId);
+            throw new IllegalArgumentException(errorLog);
         }
     }
 
     public void validateConcertHall(String concertHall) {
         if (concertHall != null && concertHall.length() >= 10) {
-            throw new IllegalArgumentException("concertHall must contains max 10 chars.");
+            var errorLog = String.format("concertHall=%s must contains max 10 chars.", concertHall);
+            throw new IllegalArgumentException(errorLog);
         }
     }
 
     public void validateEventCode(int eventCode) {
         if (String.valueOf(eventCode).length() != 3) {
-            throw new IllegalArgumentException("eventCode must contains 3 digits.");
+            var errorLog = String.format("eventCode=%d must contains 3 digits.", eventCode);
+            throw new IllegalArgumentException(errorLog);
         }
     }
 
     public void validateEventTime(long eventTime) {
         if (eventTime < 0) {
-            throw new IllegalArgumentException("eventTime must be above zero.");
+            var errorLog = String.format("eventTime=$d must be above zero.", eventTime);
+            throw new IllegalArgumentException(errorLog);
         }
     }
 
     public void validateAllowedBackpackWeight(double allowedBackpackWeight) {
         if (allowedBackpackWeight < 0) {
-            throw new IllegalArgumentException("allowedBackpackWeight must be above zero.");
+            var errorLog = String.format("allowedBackpackWeight=%d must be above zero.", allowedBackpackWeight);
+            throw new IllegalArgumentException(errorLog);
         }
     }
 
     public void validatePrice(final BigDecimal price) {
         if (price != null && price.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException("price must be above zero.");
+            var errorLog = String.format("price=%s must be above zero.", price);
+            throw new IllegalArgumentException(errorLog);
         }
     }
 }
