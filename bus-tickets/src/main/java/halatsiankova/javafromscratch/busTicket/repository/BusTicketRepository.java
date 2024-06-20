c76aa27201ad17c0007bf36b4980c316b90ca327
Veranet <halatsiankova@gmail.com>
2024-06-20 12:23:10 +0200

    lesson-6 add methods to the repository and service

--- bus-tickets/src/main/java/halatsiankova/javafromscratch/busTicket/repository/BusTicketRepository.java
+++ bus-tickets/src/main/java/halatsiankova/javafromscratch/busTicket/repository/BusTicketRepository.java
@@ -1,9 +1,13 @@
 package halatsiankova.javafromscratch.busTicket.repository;
 
+import halatsiankova.javafromscratch.busTicket.enumerated.TicketType;
 import halatsiankova.javafromscratch.busTicket.model.BusTicket;
 
+import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
@@ -19,5 +23,21 @@ public class BusTicketRepository {
     public void saveAll(Collection<BusTicket> tickets) {
         this.tickets.putAll(tickets.stream()
                 .collect(Collectors.toMap(BusTicket::getId, Function.identity())));
+    }
+
+    public Optional<BusTicket> findById(UUID ticketId) {
+        return Optional.of(tickets.get(ticketId));
+    }
+
+    public void deleteById(UUID ticketId) {
+        tickets.remove(ticketId);
+    }
+
+    public List<BusTicket> findAllByTicketTypeAndPriceFromTo(TicketType ticketType, BigDecimal priceFrom, BigDecimal priceTo) {
+        return tickets.values().stream()
+                .filter(busTicket -> busTicket.getTicketType().equals(ticketType))
+                .filter(busTicket -> busTicket.getPrice().compareTo(priceFrom) >= 0)
+                .filter(busTicket -> busTicket.getPrice().compareTo(priceTo) <= 0)
+                .toList();
     }
 }
--- bus-tickets/src/main/java/halatsiankova/javafromscratch/busTicket/service/TicketService.java
+++ bus-tickets/src/main/java/halatsiankova/javafromscratch/busTicket/service/TicketService.java
@@ -1,9 +1,12 @@
 package halatsiankova.javafromscratch.busTicket.service;
 
+import halatsiankova.javafromscratch.busTicket.enumerated.TicketType;
 import halatsiankova.javafromscratch.busTicket.model.BusTicket;
 import halatsiankova.javafromscratch.busTicket.repository.BusTicketRepository;
 
+import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 public class TicketService {
@@ -22,6 +25,20 @@ public class TicketService {
         collection.forEach(ticket -> ticket.setId(generateId()));
         repository.saveAll(collection);
         return collection;
+    }
+
+    public BusTicket getById(UUID ticketId) {
+        return repository.findById(ticketId).
+                orElseThrow(() -> new IllegalArgumentException(String.format("BusTicket with id=%s doesn't exist.",
+                        ticketId)));
+    }
+
+    public void deleteByTicketId(UUID ticketId) {
+        repository.deleteById(ticketId);
+    }
+
+    public List<BusTicket> getAllByTicketIdAndPriceFromTo(TicketType ticketType, BigDecimal priceFrom, BigDecimal priceTo) {
+        return repository.findAllByTicketTypeAndPriceFromTo(ticketType, priceFrom, priceTo);
     }
 
     private UUID generateId() {
